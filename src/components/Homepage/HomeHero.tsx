9d736f44d73dcab7ca67f247d2789f1243c5af61
Sharon <sharwong@deloitte.com.hk>
2025-04-29 10:06:16 +0800

    update

--- src/components/Homepage/HomeHero.tsx
+++ src/components/Homepage/HomeHero.tsx
@@ -3,15 +3,14 @@ import { ContainerTextFlip } from "../AceternityUI/ContainerTextFlip";
 export const HomeHero = () => {
   return (
     <div className="container mx-auto py-26">
-      <h1 className="top-title flex gap-4 *:inline py-6">
-        <p>I can</p> 
+      <h1 className="top-title py-6">
         <ContainerTextFlip
-          words={["code", "design"]}
+          words={["Hello, visitors", "I am Designer", "Also Programmer", "I make AI products"]}
         />
       </h1>
       <div className="text-xl pt-2">
         <div>Hello, I am Sharon Wong.</div>
-        <div>UI UX Designer | Frontend Developer | Based in Hong Kong</div>
+        <div>UI UX Designer | Frontend Developer | Deloitte AI Institute | Hong Kong</div>
       </div>
     </div>
   );
